a230cc8bd738173867dc59131e9d072735f3c2d6
Alex J. Malozemoff <amaloz@galois.com>
2016-07-11 13:43:22 -0700

    restrict size of omp critical as much as possible

--- src/aesrand_gmp.c
+++ src/aesrand_gmp.c
@@ -3,80 +3,78 @@
 void
 mpz_urandomb_aes(mpz_t rop, aes_randstate_t state, mp_bitcnt_t n)
 {
-#pragma omp critical
+    const mp_bitcnt_t nb = n/8+1; // number of bytes
+    unsigned char *output;
+    mp_bitcnt_t outlen = 0;
+    EVP_CIPHER_CTX *ctx;
+
+    ctx = EVP_CIPHER_CTX_new();
+    output = malloc(2 * (nb + EVP_MAX_IV_LENGTH));
+
     {
-        const mp_bitcnt_t nb = n/8+1; // number of bytes
-        unsigned char *output;
-        mp_bitcnt_t outlen = 0;
-        EVP_CIPHER_CTX *ctx;
+        unsigned char *in;
+        const int in_size = nb;
+        int final_len = 0;
 
-        // update the internal counter, works at most 2^64 times
-        memcpy(state->iv, &state->ctr, sizeof(state->ctr));
+        in = malloc(in_size);
+        memset(in, 0, in_size);
 
-        ctx = EVP_CIPHER_CTX_new();
-        EVP_EncryptInit_ex(ctx, AES_ALGORITHM, NULL, state->key, state->iv);
-
-        output = malloc(2 * (nb + EVP_MAX_IV_LENGTH));
-
+#pragma omp critical
         {
-            unsigned char *in;
-            const int in_size = nb;
-            int final_len = 0;
-
-            in = malloc(in_size);
-            memset(in, 0, in_size);
-
+            // update the internal counter, works at most 2^64 times
+            memcpy(state->iv, &state->ctr, sizeof(state->ctr));
+            EVP_EncryptInit_ex(ctx, AES_ALGORITHM, NULL, state->key, state->iv);
             while (outlen < nb) {
                 int buflen = 0;
-                EVP_EncryptUpdate(ctx, output + outlen, &buflen, in,
-                                  in_size);
+                EVP_EncryptUpdate(ctx, output + outlen, &buflen, in, in_size);
                 state->ctr++;
                 outlen += buflen;
             }
-            EVP_EncryptFinal(ctx, output + outlen, &final_len);
-            outlen += final_len;
-
-            if (outlen > nb) {
-                outlen = nb; // we will only use nb bytes
-            }
-            free(in);
         }
 
-        {
-            unsigned char *buf;
-            const mp_bitcnt_t true_len = outlen + 4;
-            mp_bitcnt_t bytelen = outlen;
-            FILE *fp;
+        EVP_EncryptFinal(ctx, output + outlen, &final_len);
+        outlen += final_len;
 
-            buf = malloc(true_len);
-            memset(buf, 0, true_len);
-            memcpy(buf + 4, output, outlen);
-            buf[4] >>= ((outlen * 8) - (unsigned int) n);
-
-            for (int i = 3; i >= 0; i--) {
-                buf[i] = (unsigned char) (bytelen % (1 << 8));
-                bytelen /= (1 << 8);
-            }
-
-            // this generates a random n-bit number.
-            fp = fmemopen(buf, true_len, "rb");
-            if (!fp) {
-                printf("Error in generating randomness.\n");
-            }
-
-            if (mpz_inp_raw(rop, fp) == 0) {
-                printf("Error in parsing randomness.\n");
-            }
-
-            fclose(fp);
-            free(buf);
+        if (outlen > nb) {
+            outlen = nb; // we will only use nb bytes
         }
-
-        free(output);
-
-        EVP_CIPHER_CTX_cleanup(ctx);
-        free(ctx);
+        free(in);
     }
+
+    {
+        unsigned char *buf;
+        const mp_bitcnt_t true_len = outlen + 4;
+        mp_bitcnt_t bytelen = outlen;
+        FILE *fp;
+
+        buf = malloc(true_len);
+        memset(buf, 0, true_len);
+        memcpy(buf + 4, output, outlen);
+        buf[4] >>= ((outlen * 8) - (unsigned int) n);
+
+        for (int i = 3; i >= 0; i--) {
+            buf[i] = (unsigned char) (bytelen % (1 << 8));
+            bytelen /= (1 << 8);
+        }
+
+        // this generates a random n-bit number.
+        fp = fmemopen(buf, true_len, "rb");
+        if (!fp) {
+            printf("Error in generating randomness.\n");
+        }
+
+        if (mpz_inp_raw(rop, fp) == 0) {
+            printf("Error in parsing randomness.\n");
+        }
+
+        fclose(fp);
+        free(buf);
+    }
+
+    free(output);
+
+    EVP_CIPHER_CTX_cleanup(ctx);
+    free(ctx);
 }
 
 void
