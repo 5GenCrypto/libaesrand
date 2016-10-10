0b2906dd63a6b632c76fc45d2851abd5f2e1fe85
Alex J. Malozemoff <amaloz@galois.com>
2016-10-10 11:36:02 -0700

    improve autotools usage
    Add --disable-flint and --disable-mpfr to configure to make using flint or mpfr
    a configure time decision

--- aesrand/aesrand.h
+++ aesrand/aesrand.h
@@ -7,11 +7,17 @@
 extern "C" {
 #endif
 
+#include "aesrand/aesrand_config.h"
+
 #include "aesrand/aesrand_init.h"
 #include "aesrand/aesrand_buf.h"
 #include "aesrand/aesrand_gmp.h"
-#include "aesrand/aesrand_flint.h"
-#include "aesrand/aesrand_mpfr.h"
+#ifdef AESRAND_HAVE_FLINT
+  #include "aesrand/aesrand_flint.h"
+#endif
+#ifdef AESRAND_HAVE_MPRF
+  #include "aesrand/aesrand_mpfr.h"
+#endif
 
 #ifdef __cplusplus
 }
