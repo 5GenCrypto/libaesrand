fa2d41346760549e737609aa774b1757fac045d4
Alex J. Malozemoff <amaloz@galois.com>
2016-10-31 13:08:02 -0400

    avoid buffer overrun when using different AES algorithm

--- aesrand/aesrand_buf.c
+++ aesrand/aesrand_buf.c
@@ -1,14 +1,20 @@
 #include "aesrand_buf.h"
 
+#include <assert.h>
 #include <string.h>
 
 unsigned char *
 random_aes(aes_randstate_t state, size_t nbits, size_t *len)
 {
-    const size_t nbytes = nbits / 8 + 1;
+    const size_t blocksize = EVP_CIPHER_CTX_block_size(state->ctx);
+    const size_t nbytes =
+        ((nbits / (blocksize * 8))
+         + ((nbits % (blocksize * 8)) ? 1 : 0)) * blocksize;
     unsigned char *output;
 
     output = malloc(nbytes + EVP_MAX_IV_LENGTH);
+
+    assert(EVP_CIPHER_iv_length(AES_ALGORITHM) != 0);
 
     {
         unsigned char in[nbytes];
@@ -21,19 +27,19 @@ random_aes(aes_randstate_t state, size_t nbits, size_t *len)
 
 #pragma omp critical
         {
-            memcpy(iv, &state->ctr, sizeof(state->ctr));
+            memcpy(iv, &state->ctr, sizeof state->ctr);
             state->ctr++;
         }
 
         /* Compute E(K, CTR, 0 || 0 || ...) */
-        EVP_EncryptInit_ex(state->ctx, AES_ALGORITHM, NULL, state->key, iv);
+        EVP_EncryptInit_ex(state->ctx, NULL, NULL, state->key, iv);
         while (outlen < nbytes) {
             int buflen = 0;
             EVP_EncryptUpdate(state->ctx, output + outlen, &buflen, in, nbytes);
             outlen += buflen;
         }
 
-        EVP_EncryptFinal(state->ctx, output + outlen, &final_len);
+        EVP_EncryptFinal_ex(state->ctx, output + outlen, &final_len);
         outlen += final_len;
 
         if (outlen > nbytes) {
--- aesrand/aesrand_init.c
+++ aesrand/aesrand_init.c
@@ -23,14 +23,13 @@ aes_randinit(aes_randstate_t rng)
         if (read(file, seed, 16) == -1) {
             fprintf(stderr, "Error reading from %s\n", RANDFILE);
             close(file);
-            return 1;
-        } else {
-            aes_randinit_seedn(rng, (char *) seed, 16, NULL, 0);
+            return AESRAND_ERR;
         }
+        aes_randinit_seedn(rng, (char *) seed, 16, NULL, 0);
     }
     if (file != -1)
         close(file);
-    return 0;
+    return AESRAND_OK;
 }
 
 void
@@ -41,12 +40,14 @@ aes_randinit_seedn(aes_randstate_t state, char *seed, size_t seed_len,
 
     state->aes_init = 1;
     state->ctr = 0;
-    state->ctx = EVP_CIPHER_CTX_new();
 
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, seed, seed_len);
     SHA256_Update(&sha256, additional, additional_len);
     SHA256_Final(state->key, &sha256);
+
+    state->ctx = EVP_CIPHER_CTX_new();
+    EVP_EncryptInit_ex(state->ctx, AES_ALGORITHM, NULL, state->key, NULL);
 }
 
 void
@@ -62,7 +63,7 @@ aes_randstate_fwrite(aes_randstate_t state, FILE *fp)
     fwrite(&state->aes_init, sizeof(state->aes_init), 1, fp);
     fwrite(&state->ctr, sizeof(state->ctr), 1, fp);
     fwrite(state->key, sizeof(state->key), 1, fp);
-    return 0;
+    return AESRAND_OK;
 }
 
 int
@@ -71,7 +72,7 @@ aes_randstate_fread(aes_randstate_t state, FILE *fp)
     fread(&state->aes_init, sizeof(state->aes_init), 1, fp);
     fread(&state->ctr, sizeof(state->ctr), 1, fp);
     fread(state->key, sizeof(state->key), 1, fp);
-    return 0;
+    return AESRAND_OK;
 }
 
 int
@@ -80,11 +81,11 @@ aes_randstate_write(aes_randstate_t state, const char *fname)
     FILE *f;
     if ((f = fopen(fname, "w")) == NULL) {
         perror(fname);
-        return 1;
+        return AESRAND_ERR;
     }
     (void) aes_randstate_fwrite(state, f);
     fclose(f);
-    return 0;
+    return AESRAND_OK;
 }
 
 int
@@ -93,9 +94,9 @@ aes_randstate_read(aes_randstate_t state, const char *fname)
     FILE *f;
     if ((f = fopen(fname, "r")) == NULL) {
         perror(fname);
-        return 1;
+        return AESRAND_ERR;
     }
     (void) aes_randstate_fread(state, f);
     fclose(f);
-    return 0;
+    return AESRAND_OK;
 }
